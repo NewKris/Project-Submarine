ff112e1413612d7329610436019bee7b94fd7a09
Kristoffer Jakobsson <kristoffer.tatel.jakobsson@gmail.com>
2025-11-27 11:57:18 +0100

    add first person camera

---
+++ Assets/Scripts/Runtime/Gameplay/PlayerCamera.cs
@@ -0,0 +1,50 @@
+using System;
+using UnityEngine;
+using Werehorse.Runtime.Utility.CommonObjects;
+
+namespace WereHorse.Runtime.Gameplay {
+    public class PlayerCamera : MonoBehaviour {
+        [Range(0, 1)] public float sensitivity;
+        public Transform yawPivot;
+        
+        [Header("Tuning")]
+        public float maxRotateSpeed;
+        
+        [Header("Pitch")]
+        [Range(0, 1)] public float pitchScale;
+        public float minPitch;
+        public float maxPitch;
+        public float pitchDamping;
+        
+        [Header("Yaw")]
+        [Range(0, 1)] public float yawScale;
+        public float yawDamping;
+
+        private DampedAngle _pitch;
+        private DampedAngle _yaw;
+
+        public float LookPitch => _pitch.Current;
+
+        public void Look(Vector2 deltaMouse) {
+            _pitch.Target -= deltaMouse.y * maxRotateSpeed * pitchScale * sensitivity;
+            _pitch.Target = Mathf.Clamp(_pitch.Target, minPitch, maxPitch);
+            
+            _yaw.Target += deltaMouse.x * maxRotateSpeed * yawScale * sensitivity;
+            _yaw.Target %= 360f;
+        }
+
+        private void Awake() {
+            _pitch = new DampedAngle(0);
+            _yaw = new DampedAngle(0);
+
+
+            yawPivot.rotation = Quaternion.Euler(0, _yaw.Current, 0);
+            transform.localRotation = Quaternion.Euler(_pitch.Current, 0, 0);
+        }
+
+        private void Update() {
+            yawPivot.rotation = Quaternion.Euler(0, _yaw.Tick(yawDamping), 0);
+            transform.localRotation = Quaternion.Euler(_pitch.Tick(pitchDamping), 0, 0);
+        }
+    }
+}
--- Assets/Scripts/Runtime/Gameplay/PlayerCharacter.cs
+++ Assets/Scripts/Runtime/Gameplay/PlayerCharacter.cs
@@ -7,23 +7,36 @@ using Werehorse.Runtime.Utility.Extensions;
 namespace WereHorse.Runtime.Gameplay {
     public class PlayerCharacter : NetworkBehaviourExtended {
         public float maxMoveSpeed;
+        public PlayerCamera playerCamera;
+        public GameObject thirdPersonModel;
 
         private CharacterController _character;
         
         private void Start() {
             DoOnNonOwners(() => {
                 enabled = false;
+                playerCamera.gameObject.SetActive(false);
             });
             
             DoOnOwner(() => {
-                enabled = true;
                 _character = GetComponent<CharacterController>();
             });
+            
+            Cursor.lockState = CursorLockMode.Locked;
         }
 
         private void Update() {
+            Look();
+            Move();
+        }
+
+        private void Move() {
             Vector3 velocity = transform.rotation * InputListener.Move.ProjectOnGround() * maxMoveSpeed;
             _character.SimpleMove(velocity);
+        }
+
+        private void Look() {
+            playerCamera.Look(InputListener.Look);
         }
     }
 }
