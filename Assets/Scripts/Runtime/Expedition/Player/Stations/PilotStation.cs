7ea763288f0415631e6f05275c3e2eefe8a433c0
Kristoffer Jakobsson <kristoffer.tatel.jakobsson@gmail.com>
2025-11-30 20:21:04 +0100

    add submarine steering

--- Assets/Scripts/Runtime/Expedition/Player/Stations/PilotStation.cs
+++ Assets/Scripts/Runtime/Expedition/Player/Stations/PilotStation.cs
@@ -1,24 +1,41 @@
 using System;
+using Unity.Netcode;
+using UnityEngine;
 using WereHorse.Runtime.Expedition.Player.Character;
+using WereHorse.Runtime.Expedition.Submarine;
 
 namespace WereHorse.Runtime.Expedition.Player.Stations {
     public class PilotStation : Station {
+        public SubmarineBody submarineBody;
+        
         public override void Activate() {
             PilotSeatInputListener.SetActive(true);
+            enabled = true;
         }
         
         public override void Deactivate() {
             PilotSeatInputListener.SetActive(false);
+            submarineBody.SendSteerValuesRpc(0, 0, 0);
+            enabled = false;
         }
 
         private void Awake() {
             PilotSeatInputListener.OnExit += Exit;
+            enabled = false;
         }
 
         private void OnDestroy() {
             PilotSeatInputListener.OnExit -= Exit;
         }
 
+        private void Update() {
+            submarineBody.SendSteerValuesRpc(
+                PilotSeatInputListener.Thrust, 
+                PilotSeatInputListener.Yaw, 
+                PilotSeatInputListener.Lift
+            );
+        }
+        
         private void Exit() {
             PlayerCharacter.ownedCharacter.DePossessStation();
         }
---
+++ Assets/Scripts/Runtime/Expedition/Submarine/SubmarineBody.cs
@@ -0,0 +1,44 @@
+using System;
+using Unity.Netcode;
+using UnityEngine;
+using WereHorse.Runtime.Common;
+
+namespace WereHorse.Runtime.Expedition.Submarine {
+    public class SubmarineBody : NetworkBehaviourExtended {
+        public Rigidbody rigidBody;
+        
+        [Header("Moving")]
+        public float maxMoveSpeed;
+        public float maxAcceleration;
+        
+        [Header("Rotating")]
+        public float maxAngularSpeed;
+        public float maxAngularAcceleration;
+
+        private float _thrust;
+        private float _yaw;
+        private float _lift;
+        
+        [Rpc(SendTo.Server)]
+        public void SendSteerValuesRpc(float thrust, float yaw, float lift) {
+            _thrust = thrust;
+            _yaw = yaw;
+            _lift = lift;
+        }
+
+        private void FixedUpdate() {
+            DoOnServer(() => {
+                Vector3 targetVel = new Vector3(0, _lift, _thrust).normalized * maxMoveSpeed;
+                targetVel = rigidBody.rotation * targetVel;
+                Vector3 nextVel = Vector3.MoveTowards(rigidBody.linearVelocity, targetVel, maxAcceleration * Time.fixedDeltaTime);
+                Vector3 delta = nextVel - rigidBody.linearVelocity;
+                rigidBody.AddForce(delta, ForceMode.VelocityChange);
+
+                Vector3 targetTorque = new Vector3(0, _yaw * maxAngularSpeed, 0);
+                Vector3 nextTorque = Vector3.MoveTowards(rigidBody.angularVelocity, targetTorque, maxAngularAcceleration * Time.fixedDeltaTime);
+                Vector3 deltaTorque = nextTorque - rigidBody.angularVelocity;
+                rigidBody.AddTorque(deltaTorque, ForceMode.VelocityChange);
+            });
+        }
+    }
+}
