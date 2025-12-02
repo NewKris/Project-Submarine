b03ba65f884f7ac8ea9aa6044578c02142bf544d
Kristoffer Jakobsson <kristoffer.tatel.jakobsson@gmail.com>
2025-12-02 23:22:00 +0100

    stuff

--- Assets/Scripts/Runtime/Expedition/Submarine/Interior.cs
+++ Assets/Scripts/Runtime/Expedition/Submarine/Interior.cs
@@ -4,27 +4,5 @@ using WereHorse.Runtime.Common;
 
 namespace WereHorse.Runtime.Expedition.Submarine {
     public class Interior : NetworkBehaviourExtended {
-        public Transform exteriorBody;
-
-        private void Update() {
-            DoOnServer(() => {
-                float roll = CalculatePitch(); 
-                float pitch = CalculatePitch();
-                
-                transform.rotation = Quaternion.Euler(pitch, 0, roll);
-            });
-        }
-
-        private float CalculateRoll() {
-            return Vector3.Angle(Vector3.up, exteriorBody.up);
-        }
-
-        private float CalculatePitch() {
-            Vector3 flatForward = exteriorBody.forward;
-            flatForward.y = 0;
-            flatForward.Normalize();
-
-            return Vector3.Angle(flatForward, exteriorBody.forward);
-        }
     }
 }
